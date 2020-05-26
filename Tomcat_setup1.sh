723bf7a2159a78c465df659242eef4b5fbe9d543
Gummudavelli sairam <65708703+Gummudavellisairam@users.noreply.github.com>
2020-05-26 17:16:44 +0530

    Update Tomcat_setup1.sh

--- Tomcat_setup1.sh
+++ Tomcat_setup1.sh
@@ -1,9 +1,9 @@
 yum install java-1.8.0
- if [$? = 0]
+ if [ $? = 0 ]
   then
-  echo "java installed successfully"
+     echo "java installed successfully"
   else
-  echo " Unable to install tomcat"
+     echo " Unable to install tomcat"
   fi
   echo $(wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.35/bin/apache-tomcat-9.0.35.tar.gz)
   echo $(tar -zxpvf apache-tomcat-9.0.35.tar.gz -C /opt)
@@ -12,3 +12,9 @@ yum install java-1.8.0
   echo $(firewall-cmd --permanent --zone=public --add-port=8080/tcp)
   echo $(firewall-cmd --reload)
   echo $(/opt/apache-tomcat-9.0.35/bin/./startup.sh)
+  if [ $? = 0 ]
+  then
+    echo" tomcat installed successfully"
+  else
+    echo " Unable to install tomcat"
+  fi
